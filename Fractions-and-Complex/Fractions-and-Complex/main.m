8aabfb7f0c8ba3a6d25af760a47ca1e024fdd74e
Stephen Franklin <stephenLfranklin@gmail.com>
2012-06-20 10:02:59 -0500

    Ch 13 E.4   functions and arrays. My problem here came down to this line: 	sum = fvalues[0]; In the function sum is allocated as a fraction but without a value. Subsequent adding to it, resulted in NAN because the [add] method involves multiplying the denominator. So had to directly preset sum to the 0th value.
    Also I don't get this __autoreleasing stuff.  And I don't get pointers, still -- especially **doubledPointers.

--- Fractions-and-Complex/Fractions-and-Complex/main.m
+++ Fractions-and-Complex/Fractions-and-Complex/main.m
@@ -6,162 +6,133 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-// test change
-
-/*
- #import "Fraction.h"
- int main (int argc, char * argv[]) {
- @autoreleasepool {
- Fraction *myFraction = [[Fraction alloc] init];
- 
- // set fraction to 1/3
- 
- [myFraction setNumerator: 1]; [myFraction setDenominator: 3];
- 
- // display the fraction
- 
- NSLog (@"The value of myFraction is:");
- [myFraction print]; 
- }
- return 0; 
- }
- 
- 
- #import "Fraction.h"
- 
- int main (int argc, char * argv[]) {
- 
- @autoreleasepool {
- Fraction *aFraction = [[Fraction alloc] init];
- 
- [aFraction setTo: 100 over: 200]; 
- [aFraction print];
- 
- [aFraction setTo: 1 over: 3];
- [aFraction print]; 
- }
- return 0; 
- }
- */
-
 #import "Fraction.h"
 #import "Complex.h"
 #import "Fraction+MathOps.h"
 #import "Fraction+Comparison.h"
 
 
+/*
+Fraction *addFractions (Fraction **f, int c);  
 
-int main (int argc, char * argv[]) {
+int main (int argc, const char * argv[])  
+{  
+    @autoreleasepool {  
+        int c;  
+        srand ( (unsigned int)time(NULL) ); //rand() seed  
+		
+        NSLog(@"How many fractions should be created to add-up?");  
+        scanf("%i", &c);  
+		
+        Fraction *__autoreleasing aF[c]; //__autoreleasing necessary for ARC release  
+        for (int i = 0; i < c ; ++i) {  
+            aF[i] = [[Fraction alloc] init];  
+            // set random fraction  
+            [aF[i] setTo: rand() % 10 over: rand() % 10];   
+			
+            do {  
+                if (aF[i].numerator == 0) {  
+                    aF[i].numerator = rand() % 10;  
+                }  
+            } while (aF[i].numerator == 0);  
+			
+            do {  
+                if (aF[i].denominator == 0) {  
+                    aF[i].denominator = rand() % 10;  
+                }  
+            } while (aF[i].denominator == 0);  
+			
+            if (aF[i].numerator > aF[i].denominator) {  
+                int temp = aF[i].numerator;  
+                aF[i].numerator = aF[i].denominator;  
+                aF[i].denominator = temp;  
+            }  
+			
+            [aF[i] print:NO];  
+        }  
+		
+        Fraction *__autoreleasing result = [[Fraction alloc] init];  
+        result = addFractions(aF, c);  
+		
+        NSLog(@"results added:");  
+        [result print:NO];  
+    }  
+    return 0;  
+}  
+
+Fraction *addFractions (Fraction **f, int c)  
+{  
+    Fraction *result = [[Fraction alloc] init];  
+    result = f[0];  
+    for (int i = 1; i < c; ++i) {  
+        result = [result add: f[i]];  
+    }  
+    return result;  
+}  
+*/
+
+Fraction *AddFracts (Fraction **fvalues, int nElements);
+
+int main (int argc, const char * argv[]) {
 	
 	@autoreleasepool {
-		
-		typedef Fraction *FractionObj;
-		
-		NSLog (@"Fractions allocated: %i", [Fraction count]);
+
+		//  Ch 13 E.4  --  
+		//  Write a function to add all the Fractions passed to it
+		//  in an array and to return the result as a Fraction.
 			   
-		FractionObj f1 = [[Fraction allocF] init],
-					f2 = [[Fraction allocF] init];
+		/*		Fraction *__autoreleasing f0 = [[Fraction alloc] init];
+		Fraction *__autoreleasing f1 = [[Fraction alloc] init];
+		Fraction *__autoreleasing f2 = [[Fraction alloc] init];
+		*/
+		 Fraction *__autoreleasing f3 = [[Fraction alloc] init];
+		/*
+		[f0 setTo: 5 over: 9];
+		[f1 setTo: 4 over: 8];
+		[f2 setTo: 3 over: 7];
+		[f3 setTo: 2 over: 6];
+*/
+		Fraction *__autoreleasing aF[3];
+		aF[0] = [[Fraction alloc] init];
+		[aF[0] setTo: 5 over: 9];
+		//		aF[0].numerator = f0.numerator;
+		//		aF[0].denominator = f0.denominator;
+		aF[1] = [[Fraction alloc] init];
+		[aF[1] setTo: 4 over: 8];
+		//		aF[1].numerator = f1.numerator;
+		//		aF[1].denominator = f1.denominator;
+		aF[2] = [[Fraction alloc] init];
+		[aF[2] setTo: 3 over: 7];
+		//		aF[2].numerator = f2.numerator;
+		//		aF[2].denominator = f2.denominator;
 		
-		Fraction *aFraction = [[Fraction allocF] init]; 
-		Fraction *bFraction = [[Fraction allocF] init];
+		//[aF[0] print:NO];
+		int i=0;
+		for (i=0; i<3; i++) {
+			[aF[i] print: NO];
+		}
 		
-		Fraction *cFraction;
+		NSLog(@"f3 is set to:");
+		[f3 print:NO];
 		
-		NSLog (@"Fractions allocated: %i", [Fraction count]);
-		// Set two fractions to 1/4 and 1/2 and add them together
+		f3 = AddFracts(aF,3);
 		
-		[aFraction setTo: -5 over: 2]; 
-		[bFraction setTo: 2 over: 7];
-		
-		// Print the results
-		
-		[aFraction print: NO]; 
-		NSLog (@"+"); 
-		[bFraction print: NO]; 
-		NSLog (@"=");
-		
-		cFraction = [aFraction add: bFraction];
-		[cFraction print: NO]; 
-		
-		NSLog (@"Fractions allocated: %i", [Fraction count]);
-		
-		cFraction = [aFraction subtract: bFraction];
-		[cFraction print: NO];
-		
-		cFraction = [aFraction multiply: bFraction];
-		[cFraction print: NO];
-		
-		cFraction = [aFraction divide: bFraction];
-		[cFraction print: NO];
-		
-		cFraction = [aFraction add: bFraction];
-		[cFraction print: NO];
-		
-		NSLog (@"\ngAddCount: %i", [Fraction addCount]);
-		
-		[aFraction print: YES]; 
-		NSLog (@"  reduced!"); 
-		[bFraction print: YES]; 
-		NSLog (@" reduced!");
-		
-		Complex *aComplex = [[Complex alloc] init];
-		
-		[aComplex setReal: 7.5 andImaginary: -2.5];
-		//		[[aComplex reduce];		// bad
-		[aComplex print];
-		
-		id data1;
-		data1 = aComplex; 
-		[data1 print];
-		id data2;
-		data2 = bFraction;
-		[data2 print: NO];
-		//		id result;
-		//		result = [data1 add: data2];	// bad
-		//		[result print];
-		
-		//  Inversion
-		[cFraction setTo: -16 over: 18];
-		[cFraction print: 1];
-		cFraction = [cFraction invert];
-		[cFraction print: 1];
-	
-
-		NSLog(@"\n-------identify-----------");
-		[f1 setTo: 3 over: 4];
-		[f2 setTo: 6 over: 8];
-		
-		NSLog(@"a is %g", [f1 convertToNum]);
-		[f1 print:NO];
-		NSLog(@"a is %g", [f2 convertToNum]);
-		[f2 print:NO];
-		NSLog(@"a is %g", [aFraction convertToNum]);
-		[aFraction print:NO];
-		NSLog(@"b is %g", [bFraction convertToNum]);
-		[bFraction print:NO];
-		NSLog(@"c is %g", [cFraction print:YES]);
-		[cFraction print:NO];
-		NSLog(@"c is %s",isnan([cFraction convertToNum])
-				 ?"Not a Number.":"a number.");
-		
-		NSLog(@"\n-------ISEqualTo-----------");
-		NSLog(@"c to c:  %i", [cFraction isEqualTo: cFraction]);
-		NSLog(@"c to a:  %i", [cFraction isEqualTo: aFraction]);
-		[cFraction setTo: 1 over: 0];
-		NSLog(@"c is set to 1/0: %f", [cFraction convertToNum]);
-		NSLog(@"c (1/0) to a:  %i", [cFraction isEqualTo: aFraction]);
-		NSLog(@"a to c (1/0):  %i", [aFraction isEqualTo: cFraction]);
-		NSLog(@"c to c:  %i", [cFraction isEqualTo: cFraction]);
-		NSLog(@"f1 to f2:  %i", [f1 isGreaterThanOrEqualTo: f2]);
-
-		NSLog(@"\n\\\\\\-------compare-----------");
-		NSLog(@"\na is %@ b",[aFraction compareSay: bFraction]);
-		NSLog(@"\nb is %@ a",[bFraction compareSay: aFraction]);
-		NSLog(@"\na is %@ c",[aFraction compareSay: cFraction]);
-		NSLog(@"\nc is %@ a",[cFraction compareSay: aFraction]);
-		NSLog(@"\nc is %@ c",[cFraction compareSay: cFraction]);
-		NSLog(@"\na is %@ a",[aFraction compareSay: aFraction]);
-		NSLog(@"\nf1 is %@ f2",[f1 compareSay: f2]);
+		NSLog(@"adding these 3 fractions:");
+		[aF[0] print:NO];
+		[aF[1] print:NO];
+		[aF[2] print:NO];
+		NSLog(@"\n equals:");
+		[f3 print:NO];
 	}
 	return 0; 
+}
+
+Fraction *AddFracts (Fraction **fvalues, int nElements)
+{
+	Fraction *sum = [[Fraction allocF] init];
+	sum = fvalues[0];
+	for (int i=1; i < nElements; i++)
+		sum = [sum add: fvalues[i]];
+	[sum reduce];
+	return sum;
 }
